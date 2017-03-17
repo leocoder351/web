0ab5bf4dcc4e7daf15362cd0009a6795b3dcd5d9
liuxuan <liuxuan351@foxmail.com>
2017-03-17 18:14:34 +0800

    Init project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import NavigationBar from '../NavigationBar/NavigationBar';
-import HomePage from '../../pages/HomePage/HomePage';
-import DrugTrace from '../../pages/DrugTrace/DrugTrace';
-import DrugApp from '../../pages/DrugApp/DrugApp';
-import PalmPharmacy from '../../pages/PalmPharmacy/PalmPharmacy';
-import AboutUs from '../../pages/AboutUs/AboutUs';
-import Recruit from '../../pages/Recruit/Recruit';
-import JobList from '../../pages/JobList/JobList';
-import JobDetail from '../../pages/JobDetail/JobDetail';
-import ContactUs from '../../pages/ContactUs/ContactUs';
-import GeneralPrac from '../../pages/GeneralPrac/GeneralPrac';
-import SkylinePrac from '../../pages/SkylinePrac/SkylinePrac';
 import './App.css';
 
 class App extends Component {
@@ -21,7 +10,7 @@ class App extends Component {
     return (
       <Router history={hashHistory}>
         <Route path="/" component={NavigationBar}>
-          <IndexRoute component={HomePage}/>
+          {/* <IndexRoute component={HomePage}/>
           <Route path="/service/drug-trace" component={DrugTrace}></Route>
           <Route path="/service/drug-app" component={DrugApp}></Route>
           <Route path="/service/palm-pharmacy" component={PalmPharmacy}></Route>
@@ -31,7 +20,7 @@ class App extends Component {
           <Route path="/recruit" component={Recruit}></Route>
           <Route path="/recruit/job-lists" component={JobList}></Route>
           <Route path="/contact-us" component={ContactUs}></Route>
-          <Route path="/recruit/job-lists/:id" component={JobDetail}></Route>
+          <Route path="/recruit/job-lists/:id" component={JobDetail}></Route> */}
         </Route>
       </Router>
     )
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import T from 'i18n-react';
 import $ from 'jquery';
-import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import Footer from '../Footer/Footer';
-import Logo from '../../images/logo.png';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router';
 import './NavigationBar.css';
 
 class NavigationBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeItem:           'home'
+    }
+
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick (e, name) {
+    this.setState({
+      activeItem:       name
+    })
+  }
 
   render() {
 
@@ -34,66 +48,18 @@ class NavigationBar extends Component {
       'contact-us':                     '联系我们',
     });
 
-    // actions
-    function onRemoveActive () {
-      console.log(1111);
-      console.log($('nav .active'));
-      $('nav .active').removeClass('active');
-    }
-
     return (
-      <div>
-        <Navbar collapseOnSelect fixedTop className="user-navbar">
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">
-                 <img className="user-navbar-logo" width={50} alt="logo" src={Logo}/>
-                 <span className="user-navbar-name">{T.translate('company-name')}</span>
-              </a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <IndexLinkContainer to="/">
-                <NavItem>{T.translate('index-page')}</NavItem>
-              </IndexLinkContainer>
-
-              <NavDropdown onClick={onRemoveActive} eventKey={2} title={T.translate('service-items')} id="basic-nav-dropdown">
-                <LinkContainer to="/service/drug-trace">
-                  <MenuItem>{T.translate('service-items.drug-trace')}</MenuItem>
-                </LinkContainer>
-                <LinkContainer to="/service/drug-app">
-                  <MenuItem>{T.translate('service-items.drug-app')}</MenuItem>
-                </LinkContainer>
-                <LinkContainer to="/service/palm-pharmacy">
-                  <MenuItem>{T.translate('service-items.palm-pharmacy')}</MenuItem>
-                </LinkContainer>
-                <LinkContainer to="/service/general-practitioner">
-                  <MenuItem>{T.translate('service-items.general-practitioner')}</MenuItem>
-                </LinkContainer>
-                <LinkContainer to="/service/skyline-practitioner">
-                  <MenuItem>{T.translate('service-items.skyline-practitioner')}</MenuItem>
-                </LinkContainer>
-              </NavDropdown>
-
-              <LinkContainer to="/about-us">
-                <NavItem>{T.translate('about-us')}</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/recruit">
-                <NavItem>{T.translate('recruit')}</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/contact-us">
-                <NavItem>{T.translate('contact-us')}</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        {this.props.children}
-
-        <Footer />
-      </div>
+      <Menu vertical>
+        <Menu.Item name='profile' as={Link} to='profile'>
+          profile
+        </Menu.Item>
+        <Menu.Item name='user' as={Link} to='user'>
+          user
+        </Menu.Item>
+        <Menu.Item name='contact' as={Link} to='contact'>
+          contact
+        </Menu.Item>
+      </Menu>
     );
   }
 }

619cdebd0f292853ec6a2aa547b2db7c93d80c29
liuxuan <liuxuan351@foxmail.com>
2017-03-22 18:23:34 +0800

    Working on nav

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,199 +2,89 @@ import React, { Component } from 'react';
 import T from 'i18n-react';
 import $ from 'jquery';
 import { Link } from 'react-router';
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 import Avatar from '../../images/avatar.jpg';
 import Signature from '../../images/signature_white.png';
 import './NavigationBar.css';
 
 class NavigationBar extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
-    this.onLinkClicked = this.onLinkClicked.bind(this);
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
 
     this.state = {
-      navNode:    null
-    };
-
-    console.log(document.body.clientWidth);
-  }
-
-  onLinkClicked(e) {
-    $('.user-links a').removeClass('active');
-    $(e.target).addClass('active');
-  }
-
-  chooseNav(clientWidth) {
-    var navNode;
-
-    if (clientWidth <= 800) {
-
-      navNode = (
-        <nav className="user-nav">
-
-            <div className="ui inverted fixed main menu">
-              <div className="ui container">
-                <a className="lanuch icon item">
-                  <i className="content icon"></i>
-                </a>
-
-                <div className="item">
-                  L e o
-                </div>
-
-                <a href="https://github.com/semantic-org/semantic-ui/" target="_blank" className="item label right">
-                  <i className="github large icon"></i>
-                  star
-                </a>
-
-
-
-              </div>
-            </div>
-
-
-        </nav>
-      );
-    } else {
-
-      navNode = (
-        <nav className="user-nav">
-
-          <div className="ui inverted vertical left fixed pointing menu">
-
-            <div className="user-info">
-              <img className="user-avatar" src={Avatar} width={80} />
-              <img className="user-signature" src={Signature} width={70} />
-            </div>
-
-            <div className="user-links" onClick={this.onLinkClicked}>
-              <Link to="/" className="item active">首页</Link>
-              <Link to="/articles" className="item">文章</Link>
-              <Link to="/works" className="item">作品</Link>
-              <Link to="/about-me" className="item">关于我</Link>
-            </div>
-
-            <div className="item">
-              <div className="ui icon input">
-                <input type="text" placeholder="Search..." />
-                <i className="circular search link icon"></i>
-              </div>
-            </div>
-
-            <div className="user-external-links ">
-              <div className="item right">
-                <a href="https://github.com/leocoder351/web/" target="_blank" className="icon right">
-                  <i className="github large icon"></i>
-                </a>
-                <a href="https://github.com/leocoder351/web/" target="_blank" className="icon right">
-                  <i className="github large icon"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      );
+      visible:          true
     }
-
-    return navNode;
   }
 
-  componentWillMount() {
-    var
-      clientWidth,
-      navNode;
-
-    clientWidth = document.body.clientWidth;
-    navNode = this.chooseNav(clientWidth);
+  toggleVisibility() {
 
     this.setState({
-      navNode:            navNode
-    });
-
-    window.onresize = function(){
-      clientWidth = document.body.clientWidth;
-      navNode = this.chooseNav(clientWidth);
-
-      this.setState({
-        navNode:            navNode
-      });
-
-      console.log(11111);
-      console.log(clientWidth);
-    }.bind(this);
+      visible:                    !this.state.visible,
+      activeItem:                 null
+    })
   }
 
+  handleItemClick(e, {name}) {
+
+    this.setState({
+      activeItem:                 name
+    })
+  }
+
+
   render() {
-    var
-      clientWidth,
-      navNode;
+    const { visible, activeItem } = this.state
+    return (
+      <div className="user-nav">
 
-    // i18n
-    T.setTexts({
+        <div className="user-info">
+          <img className="user-avatar" src={Avatar} width={80} onClick={this.toggleVisibility} />
+        </div>
 
-    });
+        <Sidebar.Pushable as={Segment} className="user-content">
+          <Sidebar className="user-links" onClick={this.changeActive} as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
+              <Link to="/" className="item">
+                <Icon name='home' />
+                首页
+              </Link>
+            </Menu.Item>
 
-    // clientWidth = document.body.clientWidth;
-    // navNode = this.chooseNav(clientWidth);
-    //
-    // this.setState({
-    //   navNode:            navNode
-    // });
-    //
-    //
-    // window.onresize = function(){
-    //   clientWidth = document.body.clientWidth;
-    //   navNode = this.chooseNav(clientWidth);
-    //
-    //   this.setState({
-    //     navNode:            navNode
-    //   });
-    //
-    //   console.log(11111);
-    //   console.log(clientWidth);
-    // }
+            <Menu.Item name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>
+              <Link to="/articles" className="item">
+                <Icon name='home' />
+                文章
+              </Link>
+            </Menu.Item>
 
-    return this.state.navNode;
+            <Menu.Item name='works' active={activeItem === 'works'} onClick={this.handleItemClick}>
+              <Link to="/works" className="item">
+                <Icon name='home' />
+                作品
+              </Link>
+            </Menu.Item>
 
+            <Menu.Item name='about-me' active={activeItem === 'about-me'} onClick={this.handleItemClick}>
+              <Link to="/about-me" className="item">
+                <Icon name='home' />
+                关于我
+              </Link>
+            </Menu.Item>
 
-    // if (clientWidth <= 800) {
-    //
-    //   return (
-    //     <div>
-    //
-    //     </div>
-    //   )
-    // } else {
-    //
-    //   return (
-    //     <nav className="user-nav">
-    //
-    //       <div className="ui inverted vertical left fixed pointing menu">
-    //
-    //         <div className="user-info">
-    //           <img className="user-avatar" src={Avatar} width={80} />
-    //           <img className="user-signature" src={Signature} width={70} />
-    //         </div>
-    //
-    //         <div className="user-links" onClick={this.onLinkClicked}>
-    //           <Link to="/" className="item active">首页</Link>
-    //           <Link to="/articles" className="item">文章</Link>
-    //           <Link to="/works" className="item">作品</Link>
-    //           <Link to="/about-me" className="item">关于我</Link>
-    //         </div>
-    //
-    //         <div className="item">
-    //           <div className="ui icon input">
-    //             <input type="text" placeholder="Search..." />
-    //             <i className="circular search link icon"></i>
-    //           </div>
-    //         </div>
-    //
-    //       </div>
-    //     </nav>
-    //   )
-    // }
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='/assets/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
   }
 }
 
